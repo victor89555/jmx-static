70cffbae28a32f04b2bb4b0d0aff225ab573874b
victor89555 <573864600@qq.com>
2018-02-06 00:57:30 +0800

    join-us page & header js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 var headerComponent = {
     data: {
-
+        isOver: false
     },
     init: function() {
         this.setClick();
@@ -13,7 +13,25 @@ var headerComponent = {
             $('#sub-navbar>ul>li').eq(i).show().siblings().hide()
         })
         $('.navbar').on('mouseout',function(){
-            $('.sub-navbar').css({'height':'0px'})
+            let timer = setTimeout(function(){
+                if(_this.data.isOver) {
+
+                }else {
+                    $('.sub-navbar').css({'height':'0px'})
+                }
+            },1500);
+        })
+        $("#sub-navbar").on('mouseover',function(){
+           _this.data.isOver = true;
+           $('.sub-navbar').css({'height':'40px'})
+        }).on('mouseout',function(){
+            let timer = setTimeout(function(){
+                if(_this.data.isOver) {
+
+                }else {
+                    $('.sub-navbar').css({'height':'0px'})
+                }
+            },1000);
         })
     }
 }
